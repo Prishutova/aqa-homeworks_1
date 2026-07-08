96fdb64d80488d624b54ea2e744e53ed839e492a
Зина <zina@MacBook-Air-Zina.local>
2026-07-08 15:12:08 +0300

    check with JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnFullBoundaryIfAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateRemainderLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroIfAmountIsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+}
